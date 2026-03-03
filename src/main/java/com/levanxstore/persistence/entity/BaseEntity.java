fbdedd0d06d171fbe2ab7c9aa52bc02a0343ce4d
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-03-02 20:32:22 -0500

    Subiendo la entidad Customer

---
+++ src/main/java/com/levanxstore/persistence/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.levanxstore.persistence.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    //Getters
+
+    public LocalDateTime getCreatedAt() {return createdAt;}
+
+    public LocalDateTime getUpdatedAt() {return updatedAt;}
+}
---
+++ src/main/java/com/levanxstore/persistence/entity/Customer.java
@@ -0,0 +1,37 @@
+package com.levanxstore.persistence.entity;
+
+import jakarta.persistence.*;
+
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    //Getters and Setters
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getFirstName() {return firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public String getPhone() {return phone;}
+
+    public void setPhone(String phone) {this.phone = phone;}
+}
