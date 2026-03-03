c81ec7a28eac0bf151997547c2eb372a6f754167
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-03-02 20:03:26 -0500

    Subiendo estructura del proyecto

---
+++ src/main/java/com/levanxstore/LevanxStoreApplication.java
@@ -0,0 +1,13 @@
+package com.levanxstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevanxStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevanxStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levanxstore/LevanxStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levanxstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevanxStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
