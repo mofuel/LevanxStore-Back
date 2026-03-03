458c718fe165596c54e1085cf5c5c4708792fc55
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-03-02 20:56:27 -0500

    Subiendo la entidad Redemption

--- src/main/java/com/levanxstore/persistence/entity/LoyaltyAccount.java
+++ src/main/java/com/levanxstore/persistence/entity/LoyaltyAccount.java
@@ -25,7 +25,6 @@ public class LoyaltyAccount extends BaseEntity{
 
     //Getters and Setters
 
-
     public Long getId() {return id;}
 
     public void setId(Long id) {this.id = id;}
---
+++ src/main/java/com/levanxstore/persistence/entity/Redemption.java
@@ -0,0 +1,37 @@
+package com.levanxstore.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "redemptions")
+public class Redemption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false)
+    private Double creditsAmount = 0.0;
+
+    //Getters and Setters
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getFirstName() {return firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public Double getCreditsAmount() {return creditsAmount;}
+
+    public void setCreditsAmount(Double creditsAmount) {this.creditsAmount = creditsAmount;}
+}
