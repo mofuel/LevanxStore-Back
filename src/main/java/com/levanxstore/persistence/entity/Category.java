84a2b721e31c53922925ac37c2c4a244b0ae3db5
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-03-04 23:12:38 -0500

    Subiendo la entidad Category

---
+++ src/main/java/com/levanxstore/persistence/entity/Category.java
@@ -0,0 +1,25 @@
+package com.levanxstore.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    //Getters and Setters
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+}
