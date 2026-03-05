6bd999d2b2765a6d4fe281a59f64d05a886cf876
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-03-04 23:42:24 -0500

    Subiendo la entidad Supplier

---
+++ src/main/java/com/levanxstore/persistence/entity/Supplier.java
@@ -0,0 +1,26 @@
+package com.levanxstore.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "suppliers")
+public class Supplier extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+
+    //Getters and Setters
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+}
