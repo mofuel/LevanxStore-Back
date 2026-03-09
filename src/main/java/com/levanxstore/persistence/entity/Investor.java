e2097673203188f9b766ff9637b94d80890f0684
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-03-09 08:49:59 -0500

    Subiendo la entidad Investor

---
+++ src/main/java/com/levanxstore/persistence/entity/Investor.java
@@ -0,0 +1,42 @@
+package com.levanxstore.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "investors")
+public class Investor extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    private String email;
+
+    //Getters and Setters
+    
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getFirstName() {return firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public String getPhone() {return phone;}
+
+    public void setPhone(String phone) {this.phone = phone;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+}
