37ccf4b1c8ad26ae0468e05bb534da877b73845f
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-08 14:12:06 -0500

    Add CategoryRepository

---
+++ src/main/java/com/levanxstore/domain/repository/CategoryRepository.java
@@ -0,0 +1,21 @@
+package com.levanxstore.domain.repository;
+
+import com.levanxstore.domain.dto.CategoryDTO;
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository {
+
+    CategoryDTO save(CategoryDTO categoryDTO);
+
+    Optional<CategoryDTO> findById(Long id);
+
+    Optional<CategoryDTO> findByName(String name);
+
+    List<CategoryDTO> findAll();
+
+    boolean existsByName(String name);
+
+    void deleteById(Long id);
+    
+}
