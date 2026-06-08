abbe261ecbe5f276280987361b1b083b77e42da1
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-08 10:58:03 -0500

    Add CustomerCrudRepository

---
+++ src/main/java/com/levanxstore/persistence/crud/CustomerCrudRepository.java
@@ -0,0 +1,10 @@
+package com.levanxstore.persistence.crud;
+
+import com.levanxstore.persistence.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface CustomerCrudRepository extends JpaRepository<Customer, Long> {
+    
+    Optional<Customer> findByPhone(String phone);
+}
