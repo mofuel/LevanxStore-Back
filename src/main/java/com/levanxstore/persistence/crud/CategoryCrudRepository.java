b41bf90d786e84f0b0aac074445ff71bb819b62b
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-08 10:47:31 -0500

    Add CategoryCrudRepository

---
+++ src/main/java/com/levanxstore/persistence/crud/CategoryCrudRepository.java
@@ -0,0 +1,12 @@
+package com.levanxstore.persistence.crud;
+
+import com.levanxstore.persistence.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+
+public interface CategoryCrudRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+    boolean existsByName(String name);
+
+}
