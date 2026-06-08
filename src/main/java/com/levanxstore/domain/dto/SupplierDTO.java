8337fe8d5241dd8e28daba37774b04e5fb7d5dc9
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-08 09:27:48 -0500

    Add SupplierDTO

---
+++ src/main/java/com/levanxstore/domain/dto/SupplierDTO.java
@@ -0,0 +1,16 @@
+package com.levanxstore.domain.dto;
+
+public class SupplierDTO {
+
+    private Long id;
+    private String name;
+
+    //Getters and Setters
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    
+}
