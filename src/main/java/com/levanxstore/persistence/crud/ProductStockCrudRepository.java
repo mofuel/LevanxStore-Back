e80bc2c55bced6524929b8fcb72aca9869d83b74
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-08 10:52:28 -0500

    Add ProductStockCrudRepository

---
+++ src/main/java/com/levanxstore/persistence/crud/ProductStockCrudRepository.java
@@ -0,0 +1,12 @@
+package com.levanxstore.persistence.crud;
+
+import com.levanxstore.persistence.entity.ProductStock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductStockCrudRepository extends JpaRepository<ProductStock, Long> {
+    List<ProductStock> findByProductId(Long productId);
+    List<ProductStock> findByModelId(Long modelId);
+    Optional<ProductStock> findByProductIdAndModelId(Long productId, Long modelId);
+}
