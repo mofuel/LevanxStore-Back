7874034bbf4148dccf8d8f840db10306b66cb52b
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-08 11:11:46 -0500

    Add ExpenseCrudCrudRepository

---
+++ src/main/java/com/levanxstore/persistence/crud/ExpenseCrudRepository.java
@@ -0,0 +1,12 @@
+package com.levanxstore.persistence.crud;
+
+import com.levanxstore.persistence.entity.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ExpenseCrudRepository extends JpaRepository<Expense, Long> {
+
+    List<Expense> findBySupplierId(Long supplierId);
+    
+    List<Expense> findByStatus(Expense.ExpenseStatus status);
+}
--- src/main/java/com/levanxstore/persistence/crud/SaleItemCrudRepository.java
+++ src/main/java/com/levanxstore/persistence/crud/SaleItemCrudRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface SaleItemCrudRepository extends JpaRepository<SaleItem, Long> {
 
     List<SaleItem> findBySaleId(Long saleId);
-    
+
 }
