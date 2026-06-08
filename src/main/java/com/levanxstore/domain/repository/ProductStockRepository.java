5f06ebfca9f00a546a89d2af8118b50b1c25ccd2
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-08 14:16:42 -0500

    Add ProductStockRepository

---
+++ src/main/java/com/levanxstore/domain/repository/ProductStockRepository.java
@@ -0,0 +1,23 @@
+package com.levanxstore.domain.repository;
+
+import com.levanxstore.domain.dto.ProductStockDTO;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductStockRepository {
+
+    ProductStockDTO save(ProductStockDTO productStockDTO);
+
+    Optional<ProductStockDTO> findById(Long id);
+
+    List<ProductStockDTO> findAll();
+
+    List<ProductStockDTO> findByProductId(Long productId);
+
+    List<ProductStockDTO> findByModelId(Long modelId);
+
+    Optional<ProductStockDTO> findByProductIdAndModelId(Long productId, Long modelId);
+
+    void deleteById(Long id);
+    
+}
